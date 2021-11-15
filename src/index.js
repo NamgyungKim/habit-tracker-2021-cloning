5252569b6a0fc07583f71d5509edbf523db13839
NamgyungKim <namgyung.kim@gmail.com>
2021-11-15 21:01:34 +0900

    Add: habit component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';      
 import App from './app';
+import '@fortawesome/fontawesome-free/js/all.js'
 
 ReactDOM.render(
-    <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
